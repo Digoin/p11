c1d68f51ee15f4cf378b26804c2aaf8e38ede8c5
digoin <titouan.dechanet@teamwork.net>
2021-11-23 17:16:45 +0100

    few modifications

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -147,3 +147,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL ='/'
+
+
+AUTH_USER_MODEL = 'user_management.UserExtension'
--- pur_beurre/user_management/models.py
+++ pur_beurre/user_management/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
-class UserExtension(User):
+class UserExtension(AbstractUser):
     pass
