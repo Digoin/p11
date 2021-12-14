599869b2713d53260cd44aaa1380b220049218c3
digoin <titouan.dechanet@teamwork.net>
2021-12-14 15:20:04 +0100

    WIP: fix all tiny problems

--- pur_beurre/main_site/urls.py
+++ pur_beurre/main_site/urls.py
@@ -6,4 +6,5 @@ app_name = 'main_site'
 urlpatterns = [
     path('', views.home, name='home'),
     path('aliment/<product_id>', views.product_description, name='product'),
+    path('notice/', views.legal_notice, name='notice'),
 ]
