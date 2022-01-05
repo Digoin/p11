64e2179b883464f0365d2e958789b92da94e4c13
digoin <titouan.dechanet@teamwork.net>
2022-01-05 19:57:11 +0100

    sorted by best produces

--- pur_beurre/main_site/views.py
+++ pur_beurre/main_site/views.py
@@ -12,21 +12,25 @@ def legal_notice(request):
   
 def product_description(request, product_id):
     substitute_products = []
+    best_subsitute_products = []
+    nutriscore_count = 0
     product = Product.objects.get(id=product_id)
     for categories in product.category_set.all():
-        if len(substitute_products) == 6:
-            break
         for linked_product in categories.products.all():
-            if len(substitute_products) == 6:
-                break
-            if string.ascii_uppercase.index(linked_product.nutriscore) <= string.ascii_uppercase.index(product.nutriscore) and linked_product not in substitute_products:
+            if string.ascii_uppercase.index(linked_product.nutriscore) <= string.ascii_uppercase.index(product.nutriscore) and linked_product not in substitute_products and linked_product != product:
                 substitute_products.append(linked_product)
-    print(substitute_products)
+    while nutriscore_count < 4:
+        for best_product in substitute_products:
+            if len(best_subsitute_products) >= 6:
+                break
+            if string.ascii_uppercase.index(best_product.nutriscore) == nutriscore_count:
+                best_subsitute_products.append(best_product)
+        nutriscore_count += 1
     context = {
         "product_name" : product.name,
         "product_nutriscore" : product.nutriscore,
         "product_url" : product.url,
         "product_image" : product.img_url,
-        "substitute_products" : substitute_products,
+        "substitute_products" : best_subsitute_products,
     }
     return render(request, 'main_site/product_description_page.html', context)
