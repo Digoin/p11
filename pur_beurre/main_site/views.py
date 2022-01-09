d41264aa4399b2fca9a3f1257350eaabf88130a2
digoin <titouan.dechanet@teamwork.net>
2022-01-09 17:44:29 +0100

    research logic is ok

--- pur_beurre/main_site/views.py
+++ pur_beurre/main_site/views.py
@@ -1,6 +1,8 @@
+import string
+
 from django.shortcuts import render
 from main_site.models import Product
-import string
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 # Create your views here.
 
@@ -14,11 +16,14 @@ def product_description(request, product_id):
     substitute_products = []
     best_subsitute_products = []
     nutriscore_count = 0
+
     product = Product.objects.get(id=product_id)
+
     for categories in product.category_set.all():
         for linked_product in categories.products.all():
             if string.ascii_uppercase.index(linked_product.nutriscore) <= string.ascii_uppercase.index(product.nutriscore) and linked_product not in substitute_products and linked_product != product:
                 substitute_products.append(linked_product)
+    
     while nutriscore_count < 4:
         for best_product in substitute_products:
             if len(best_subsitute_products) >= 6:
@@ -26,6 +31,7 @@ def product_description(request, product_id):
             if string.ascii_uppercase.index(best_product.nutriscore) == nutriscore_count:
                 best_subsitute_products.append(best_product)
         nutriscore_count += 1
+
     context = {
         "product_name" : product.name,
         "product_nutriscore" : product.nutriscore,
@@ -37,4 +43,18 @@ def product_description(request, product_id):
         "product_sugar" : product.sugar,
         "substitute_products" : best_subsitute_products,
     }
+
     return render(request, 'main_site/product_description_page.html', context)
+
+def product_research(request, research):
+    no_repetition_result = []
+    vectors = SearchVector('name', weight='A') + SearchVector('category__name', weight='B')
+    query = SearchQuery(f'{research}')
+
+    research_result = Product.objects.annotate(rank=SearchRank(vectors, query)).order_by('-rank')
+
+    for product in research_result:
+        if product not in no_repetition_result:
+            no_repetition_result.append(product)
+
+    return render(request, 'main_site/product_research.html', no_repetition_result)
