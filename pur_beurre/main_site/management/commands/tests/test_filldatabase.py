2eebfdedd180f18c6e73538eb813ccac9e1711ab
digoin <titouan.dechanet@teamwork.net>
2022-01-25 13:24:46 +0100

    waiting for stackoverflox answers

--- pur_beurre/main_site/management/commands/tests/test_filldatabase.py
+++ pur_beurre/main_site/management/commands/tests/test_filldatabase.py
@@ -1,22 +1,31 @@
 from django.test import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+from json import dumps
+
 from ..filldatabase import Command
+from .._product_maker import ApiProduct
 
 class TestCommand(TestCase):
 
-    @patch('self.product_data_validity')
-    @patch('ApiProduct')
+    def setUp(self):
+        self.command = Command()
+
+
     @patch('requests.get')
-    def test_products_list_creator(self, mock_get, mock_product, mock_validity):
-        mock_get.return_value = {"products":
-        {
-            "name": "apple",
-            "categories": "apple,red"
-        }
-        }
-        mock_product.return_value = "test"
-        mock_validity.return_value = True
+    def test_products_list_creator(self, mock_get):
+                
+        mock_get.return_value = dumps({"products":
+            {
+                "name": "apple",
+                "categories": "fruit,red"
+            }
+        })
 
-        command = Command
+        api_product = ApiProduct
+        api_product.name = MagicMock(return_value="apple")
+        api_product.categories = MagicMock(return_value="fruit, red")
 
-        assert ["test","test"] == command.products_list_creator(None,None)
+        self.command.product_data_validity = MagicMock(return_value=True)
+
+        assert ["apple","fruit, red"] == self.command.products_list_creator(None,None)
+
