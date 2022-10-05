7d5ca9d0557882bdf384d43289eb5bd141ad7378
Digoin <titouan.dechanet@outlook.com>
2022-10-05 13:57:14 +0200

    do database settings

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -81,14 +81,7 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
 
