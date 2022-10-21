870e3ba861ff3f8bc184b1d078328cedc0a97643
Digoin <titouan.dechanet@outlook.com>
2022-10-21 13:32:04 +0200

    No need for envavar with debug

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default="False") 
+DEBUG = False
 
 ALLOWED_HOSTS = ['164.90.173.170', 'localhost']
 
